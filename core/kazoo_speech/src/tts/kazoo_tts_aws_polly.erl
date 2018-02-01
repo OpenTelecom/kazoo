8511b3c4f0238b0b7f82156f5486e51efef46d99
serg-i-o <s.yermolaev@gmail.com>
2018-02-01 22:36:13 +0700

    awl polly docs and fix

--- core/kazoo_speech/src/tts/kazoo_tts_aws_polly.erl
+++ core/kazoo_speech/src/tts/kazoo_tts_aws_polly.erl
@@ -4,43 +4,114 @@
 
 -export([
       create/4
-    , set_api_key/1
-    , check_aws_config/0
+    , set_api_key/1, set_aws_key_id/1 , set_aws_secret_key/1
     , get_aws_config/0
+    , check_aws_config/0
     , check_voice/0
-    , set_aws_key_id/1
-    , set_aws_secret_key/1
 ]).
 
 -include("kazoo_speech.hrl").
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
 
-%%{
-%%    "data": {
-%%        "default": {
-%%            "tts_aws_polly_url": "https://polly.eu-west-2.amazonaws.com/v1/speech",
-%%            "tts_aws_key_id": "AKIAIFHXKQEEHHMCT77Q",
-%%            "tts_aws_secret_key": "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",
-%%            "tts_speed": 22050,
-%%            "tts_media_format": "mp3",
-%%            "tts_provider": "aws_polly",
-%%            "tts_api_key": "blank_aws_api_key"
-%%        },
-%%        "id": "speech"
-%%    }
-%%}
 
--define(ISPEECH_VOICE_MAPPINGS
-       ,[{<<"female/en-us">>, <<"Kimberly">>}
-        ,{<<"male/en-us">>,   <<"Justin">>}
-        ,{<<"female/en-ca">>, <<"Kimberly">>}
-        ,{<<"male/en-ca">>,   <<"Justin">>}
-        ,{<<"female/en-au">>, <<"Nicole">>}
-        ,{<<"male/en-au">>,   <<"Russell">>}
-        ,{<<"female/en-gb">>, <<"Amy">>}
-        ,{<<"male/en-gb">>,   <<"Brian">>}
-        ]
-       ).
+-define(AWS_VOICE_MAPPINGS,[
+     {<<"female/da-dk">>, <<"Naja">>}
+    ,{<<"female/da-dk/naja">>, <<"Naja">>}
+    ,{<<"male/da-dk/mads">>, <<"Mads">>}
+    ,{<<"female/nl-nl">>, <<"Lotte">>}
+    ,{<<"female/nl-nl/lotte">>, <<"Lotte">>}
+    ,{<<"male/nl-nl/ruben">>, <<"Ruben">>}
+    ,{<<"female/en-au">>, <<"Nicole">>}
+    ,{<<"female/en-au/nicole">>, <<"Nicole">>}
+    ,{<<"male/en-au/russell">>, <<"Russell">>}
+    ,{<<"female/en-gb">>, <<"Amy">>}
+    ,{<<"male/en-gb">>, <<"Brian">>}
+    ,{<<"female/en-gb/amy">>, <<"Amy">>}
+    ,{<<"male/en-gb/brian">>, <<"Brian">>}
+    ,{<<"female/en-gb/emma">>, <<"Emma">>}
+    ,{<<"female/en-in/aditi">>, <<"Aditi">>}
+    ,{<<"female/en-in/raveena">>, <<"Raveena">>}
+    ,{<<"female/en-us">>, <<"Ivy">>}
+    ,{<<"male/en-us">>, <<"Joey">>}
+    ,{<<"female/en-us/ivy">>, <<"Ivy">>}
+    ,{<<"female/en-us/joanna">>, <<"Joanna">>}
+    ,{<<"male/en-us/joey">>, <<"Joey">>}
+    ,{<<"male/en-us/justin">>, <<"Justin">>}
+    ,{<<"female/en-us/kendra">>, <<"Kendra">>}
+    ,{<<"female/en-us/kimberly">>, <<"Kimberly">>}
+    ,{<<"male/en-us/matthew">>, <<"Matthew">>}
+    ,{<<"female/en-us/salli">>, <<"Salli">>}
+    ,{<<"female/en-ca">>, 'undefined'}
+    ,{<<"male/en-ca/geraint">>, <<"Geraint">>}
+    ,{<<"male/en-gb-wls/geraint">>, <<"Geraint">>}
+    ,{<<"female/fr-fr">>, <<"Celine">>}
+    ,{<<"male/fr-fr">>, <<"Mathieu">>}
+    ,{<<"female/fr-fr/celine">>, <<"Celine">>}
+    ,{<<"male/fr-fr/mathieu">>, <<"Mathieu">>}
+    ,{<<"female/fr-ca">>, <<"Chantal">>}
+    ,{<<"male/fr-ca">>, 'undefined'}
+    ,{<<"female/fr-ca/chantal">>, <<"Chantal">>}
+    ,{<<"female/de-de">>, <<"Marlene">>}
+    ,{<<"male/de-de">>, <<"Hans">>}
+    ,{<<"female/de-de/marlene">>, <<"Marlene">>}
+    ,{<<"male/de-de/hans">>, <<"Hans">>}
+    ,{<<"female/de-de/vicki">>, <<"Vicki">>}
+    ,{<<"female/is-is/dora">>, <<"Dora">>}
+    ,{<<"male/is-is/karl">>, <<"Karl">>}
+    ,{<<"female/it-it">>, <<"Carla">>}
+    ,{<<"male/it-it">>, <<"Giorgio">>}
+    ,{<<"female/it-it/carla">>, <<"Carla">>}
+    ,{<<"male/it-it/giorgio">>, <<"Giorgio">>}
+    ,{<<"female/ja-jp">>, <<"Mizuki">>}
+    ,{<<"male/ja-jp">>, <<"Takumi">>}
+    ,{<<"female/ja-jp/mizuki">>, <<"Mizuki">>}
+    ,{<<"male/ja-jp/takumi">>, <<"Takumi">>}
+    ,{<<"female/ko-kr">>, <<"Seoyeon">>}
+    ,{<<"male/ko-kr">>, 'undefined'}
+    ,{<<"female/ko-kr/seoyeon">>, <<"Seoyeon">>}
+    ,{<<"female/nb-no">>, <<"Liv">>}
+    ,{<<"female/nb-no/liv">>, <<"Liv">>}
+    ,{<<"female/pl-pl">>, <<"Ewa">>}
+    ,{<<"female/pl-pl/ewa">>, <<"Ewa">>}
+    ,{<<"female/pl-pl/maja">>, <<"Maja">>}
+    ,{<<"male/pl-pl/jacek">>, <<"Jacek">>}
+    ,{<<"male/pl-pl/jan">>, <<"Jan">>}
+    ,{<<"female/pt-br">>, <<"Vitoria">>}
+    ,{<<"female/pt-br/vitoria">>, <<"Vitoria">>}
+    ,{<<"male/pt-br/ricardo">>, <<"Ricardo">>}
+    ,{<<"female/pt-pt">>, <<"Ines">>}
+    ,{<<"male/pt-pt">>, <<"Cristiano">>}
+    ,{<<"female/pt-pt/ines">>, <<"Ines">>}
+    ,{<<"male/pt-pt/cristiano">>, <<"Cristiano">>}
+    ,{<<"female/ro-ro/carmen">>, <<"Carmen">>}
+    ,{<<"female/ru-ru">>, <<"Tatyana">>}
+    ,{<<"male/ru-ru">>, <<"Maxim">>}
+    ,{<<"female/ru-ru/tatyana">>, <<"Tatyana">>}
+    ,{<<"male/ru-ru/maxim">>, <<"Maxim">>}
+    ,{<<"female/es-es">>, <<"Conchita">>}
+    ,{<<"male/es-es">>, <<"Enrique">>}
+    ,{<<"female/es-es/conchita">>, <<"Conchita">>}
+    ,{<<"male/es-es/enrique">>, <<"Enrique">>}
+    ,{<<"female/es-us">>, <<"Penelope">>}
+    ,{<<"male/es-us">>, <<"Miguel">>}
+    ,{<<"female/es-us/penelope">>, <<"Penelope">>}
+    ,{<<"male/es-us/miguel">>, <<"Miguel">>}
+    ,{<<"female/ca-es">>, 'undefined'}
+    ,{<<"female/sv-se">>, <<"Astrid">>}
+    ,{<<"female/sv-se/astrid">>, <<"Astrid">>}
+    ,{<<"female/tr-tr">>, <<"Filiz">>}
+    ,{<<"male/tr-tr">>, 'undefined'}
+    ,{<<"female/tr-tr/filiz">>, <<"Filiz">>}
+    ,{<<"female/cy-gb/gwyneth">>, <<"Gwyneth">>}
+    ,{<<"female/cs-cz">>, 'undefined'}
+    ,{<<"female/hu-hu">>, 'undefined'}
+    ,{<<"female/fi-fi">>, 'undefined'}
+    ,{<<"female/zh-cn">>, 'undefined'}
+    ,{<<"male/zh-cn">>, 'undefined'}
+    ,{<<"female/zh-hk">>, 'undefined'}
+    ,{<<"female/zh-tw">>, 'undefined'}
+    ,{<<"female/us-us">>, 'undefined'}]
+).
 
 -define(AWS_POLLY_SPEECH_METHOD, 'post').
 
@@ -66,7 +137,7 @@ set_aws_secret_key(Key) ->
 
 -spec create(kz_term:ne_binary(), kz_term:ne_binary(), kz_term:ne_binary(), kz_term:proplist()) -> create_resp().
 create(Text, Voice, Format, Options) ->
-    VoiceMappings = ?ISPEECH_VOICE_MAPPINGS,
+    VoiceMappings = ?AWS_VOICE_MAPPINGS,
 
     case props:get_value(kz_term:to_lower_binary(Voice), VoiceMappings) of
         'undefined' ->
@@ -78,10 +149,14 @@ create(Text, Voice, Format, Options) ->
 -spec make_request(kz_term:ne_binary(), kz_term:ne_binary(), kz_term:ne_binary(), kz_term:proplist()) -> create_resp().
 make_request(Text, VoiceId, Format, Options) ->
     BaseUrl = kapps_config:get_string(?MOD_CONFIG_CAT, <<"tts_aws_polly_url">>),
-
+    case http_uri:parse(BaseUrl) of
+        {'ok', {_ProtocolAtom,_UserInfo, Host, _Port, Path, _Query}} ->
+            make_request(Text, VoiceId, Format, Options, BaseUrl, Host, Path);
+        _ -> {'error', 'invalid_aws_service_url'}
+    end.
+make_request(Text, VoiceId, Format, Options, BaseUrl, Host, Path) ->
     {Service, Config} = get_aws_config(),
     Method = ?AWS_POLLY_SPEECH_METHOD,
-    {'ok', {_ProtocolAtom,_UserInfo, Host, _Port, Path, _Query}} = http_uri:parse(BaseUrl),
     Region = Config#aws_config.aws_region,
 
     Props = [
