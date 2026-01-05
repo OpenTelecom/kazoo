e2e8e9781fbd523afee6d2b841aa928487a0e971
Rob Nichols <robert.nichols@dialwave.com>
2026-01-05 12:11:12 -0500

    openkazoo: initial support for OTP26/27
    Signed-off-by: Rob Nichols <robert.nichols@dialwave.com>

--- applications/konami/src/module/konami_transfer.erl
+++ applications/konami/src/module/konami_transfer.erl
@@ -136,7 +136,7 @@ handle(Data, Call) ->
         _ -> 'ok'
     catch
         'exit':'normal' -> 'ok';
-        ?STACKTRACE(_E, _R, ST)
+        _E:_R:ST ->
             lager:info("statem terminated abnormally: ~s: ~p", [_E, _R]),
             kz_util:log_stacktrace(ST)
     end.
--- applications/tasks/src/modules/kt_task_worker.erl
+++ applications/tasks/src/modules/kt_task_worker.erl
@@ -226,7 +226,7 @@ is_task_successful(MappedRow
             %% Stop on crashes, but only skip typefailed rows.
             {'false', Columns, Written, IterValue}
     catch
-        ?STACKTRACE(_, _R, ST)
+        _:_R:ST ->
             lager:error("verifier crashed: ~p", [_R]),
             kz_util:log_stacktrace(ST),
             {Columns,Written} = store_return(State, MappedRow, ?WORKER_TASK_MAYBE_OK),
